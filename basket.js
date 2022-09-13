daadea5e7339205616e7109c7d4d2a9ec53d42b3
Sergey Barutkin <btkins@gmail.com>
2022-09-13 11:53:56 +0200

    добавлен первоначальный проект интернет магазина.

---
+++ basket.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// const a = 'Hello';
+// console.log(`${a} world`);
+//
+// let cardsEls = document.querySelectorAll('.card');
+// console.log(cardsEls);
+// let buttonAddEls = document.querySelectorAll('.buttonAddBasket');
+// buttonAddEls.forEach(button){
+//     button.addEventListener('click', avent)
+// }
+// console.log(buttonAddEls);
+let basketClassEl = document.querySelector('.basket');
+console.log(basketClassEl);
+let basketIconEl = document.querySelectorAll('.basketIconWrap');
+basketIconEl.addEventListener('click', event {
+basketClassEl.class.hidden = false;
+}
+// console.log(basketIconEl);
+
